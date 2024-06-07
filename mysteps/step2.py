0a57d8497c0a7256825a82419cc7b483949e615a
Naoyuki <shig.feu@gmail.com>
2024-06-08 01:49:12 +0900

    dotの作成

--- mysteps/step2.py
+++ mysteps/step2.py
@@ -3,6 +3,7 @@ if '__file__' in globals():
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import numpy as np
 from mydezero import Variable
+from mydezero.utils import get_dot_graph
 
 def sphere(x,y):
     z = x ** 2 + y ** 2
@@ -12,8 +13,21 @@ def mytans(x,y):
     z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
     return z
 
-x = Variable(np.array(1.0))
-y = Variable(np.array(1.0))
+x0 = Variable(np.array(1.0))
+x1 = Variable(np.array(1.0))
+y = x0 + x1
+
+x0.name = 'x0'
+x1.name = 'x1'
+y.name = 'y'
+
+txt = get_dot_graph(y, verbose = False)
+print(txt)
+
+with open('sample.dot', 'w') as o:
+    o.write(txt)
+
+
 z = mytans(x,y)
 z.backward()
 
