6314d4d3986d4531f506863c25047b537cab9a23
matsuyama <shig.feu@gmail.com>
2024-06-08 18:23:53 +0900

    Tanhの実装

--- mysteps/step2.py
+++ mysteps/step2.py
@@ -7,6 +7,9 @@ from mydezero import Function
 from mydezero.utils import plot_dot_graph
 import mydezero.functions as F
 import math
+import matplotlib.pyplot as plt
+
+
 def sphere(x,y):
     z = x ** 2 + y ** 2
     return z
@@ -15,49 +18,14 @@ def mytans(x,y):
     z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
     return z
 
-class Sin(Function):
-    def forward(self, x):
-        y = np.sin(x)
-        return y
-    
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = gy * np.cos(x)
-        return gx
-
-def sin(x):
-    return Sin()(x)
-
-def my_sin(x, threshold=0.0001):
-    y = 0
-    for i in range(100000):
-        c = (-1) ** i / math.factorial(2 * i + 1)
-        t = c * x ** (2 * i + 1)
-        y = y + t
-        if abs(t.data) < threshold:
-            break
-    return y
-
-def rosenbrock(x0, x1):
-    y = 100 * (x1 - x0 ** 2)**2 + (x0 - 1) ** 2
-    return y
-
-def f(x):
-    y = x ** 4 - 2 * x ** 2
-    return y
 
 
-x = Variable(np.array(1.0))
-y = F.sin(x)
+x = Variable(np.array(2.0))
+y = x**2
 y.backward(create_graph=True)
+gx = x.grad
+x.cleargrad()
 
-for i in range(3):
-    gx = x.grad
-    x.cleargrad()
-    gx.backward(create_graph=True)
-    print(x.grad)
-
-
-
-
-
+z = gx ** 3 + y
+z.backward()
+print(x.grad)
