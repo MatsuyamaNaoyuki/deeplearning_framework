6ed1719271ea83c6a15aa38cb9ba7012e1c54665
matsuyama <shig.feu@gmail.com>
2024-06-08 13:37:22 +0900

    変数と関数のdotファイルの作成関数を作成

---
+++ mydezero/utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+import unittest
+import weakref
+import contextlib
+
+def _dot_var(v, verbose=False):
+    dot_var = '{} [label="{}", color = orange, style = filled]\n'
+    name = '' if v.name is None else v.name
+    if verbose and v.data is not None:
+        if v.name is not None:
+            name += ': '
+        name += str(v.shape) + ' ' + str(v.dtype)
+    return dot_var.format(id(v), name) #ここで名前をいれてる
+
+def _dot_func(f):
+    dot_func = '{} [label="{}", color = lightbule, style = filled, shape=box]\n'
+    txt = dot_func.format(id(f), f.__class__.__name__)
+
+    dot_edge= '{} -> {}\n'
+    for x in f.inputs:
+        txt += dot_edge.format(id(x), id(f))
+    for y in f.outputs:
+        txt += dot_edge.format(id(f), id(y()))
+    return txt
+
+
--- mysteps/step2.py
+++ mysteps/step2.py
@@ -3,32 +3,41 @@ if '__file__' in globals():
     sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 import numpy as np
 from mydezero import Variable
-from mydezero.utils import get_dot_graph
+from mydezero.utils import _dot_var
+from mydezero.utils import _dot_func
+# def sphere(x,y):
+#     z = x ** 2 + y ** 2
+#     return z
 
-def sphere(x,y):
-    z = x ** 2 + y ** 2
-    return z
+# def mytans(x,y):
+#     z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
+#     return z
 
-def mytans(x,y):
-    z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
-    return z
+# x0 = Variable(np.array(1.0))
+# x1 = Variable(np.array(1.0))
+# y = x0 + x1
+
+# x0.name = 'x0'
+# x1.name = 'x1'
+# y.name = 'y'
+
+# txt = get_dot_graph(y, verbose = False)
+# print(txt)
+
+# with open('sample.dot', 'w') as o:
+#     o.write(txt)
+
+
+# z = mytans(x,y)
+# z.backward()
+
+# print(x.grad, y.grad)
 
 x0 = Variable(np.array(1.0))
 x1 = Variable(np.array(1.0))
 y = x0 + x1
-
-x0.name = 'x0'
-x1.name = 'x1'
-y.name = 'y'
-
-txt = get_dot_graph(y, verbose = False)
+txt = _dot_func(y.creator)
 print(txt)
-
-with open('sample.dot', 'w') as o:
-    o.write(txt)
-
-
-z = mytans(x,y)
-z.backward()
-
-print(x.grad, y.grad)
+x0.name = 'x0'
+print(_dot_var(x0))
+print(_dot_var(x0, verbose=True))
